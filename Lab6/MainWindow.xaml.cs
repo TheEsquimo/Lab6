fa7b3e6fee23a707814f53a7d8c4e945d6351d03
Elvis <elvostudios@hotmail.com>
2019-10-21 12:18:53 +0200

    Set up blocking collections and start-up variables/classes

--- Lab6/MainWindow.xaml.cs
+++ Lab6/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,29 @@ namespace Lab6
     /// </summary>
     public partial class MainWindow : Window
     {
+        double timeTillBarCloses;
+        int glassAmount = 8;
+        int chairAmount = 9;
+        BlockingCollection<Glass> glassShelf;
+        BlockingCollection<Glass> dirtyGlasses;
+        ConcurrentQueue<Guest> guestsWaitingForBeer;
+        ConcurrentQueue<Guest> guestsWaitingForSeat;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            glassShelf = new BlockingCollection<Glass>();
+            for (int i = 0; i < glassAmount; i++)
+            {
+                Glass newGlass = new Glass();
+                glassShelf.Add(newGlass);
+            }
+            glassShelf.CompleteAdding();
+
+            dirtyGlasses = new BlockingCollection<Glass>();
+            guestsWaitingForBeer = new ConcurrentQueue<Guest>();
+            guestsWaitingForSeat = new ConcurrentQueue<Guest>();
         }
     }
 }
